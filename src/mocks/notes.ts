f0be31e8b25cbf4d2fd903fa75e832a163af4359
Arthur S. Silva <arthur.santos.off@gmail.com>
2023-10-26 02:35:59 -0300

    chore: create a notes mock

---
+++ src/mocks/notes.ts
@@ -0,0 +1,34 @@
+export const mockNotes: INotes[] = [
+  {
+    id: "1",
+    category: "Work",
+    title: "Reunião de equipe",
+    description: "Reunião para discutir o andamento do projeto X.",
+    createdAt: new Date("2023-10-25T09:00:00"),
+    isChecked: true,
+  },
+  {
+    id: "2",
+    category: "Home",
+    title: "Comprar mantimentos",
+    description: "Comprar leite, ovos, pão e frutas.",
+    createdAt: new Date("2023-10-25T15:30:00"),
+    isChecked: true,
+  },
+  {
+    id: "3",
+    category: "Personal",
+    title: "Assistir a um filme",
+    description: "Assistir ao filme 'O Poderoso Chefão'.",
+    createdAt: new Date("2023-10-26T20:00:00"),
+    isChecked: false,
+  },
+  {
+    id: "4",
+    category: "Work",
+    title: "Estudar para o exame",
+    description: "Revisar apontamentos e fazer exercícios práticos.",
+    createdAt: new Date("2023-10-27T14:00:00"),
+    isChecked: false,
+  },
+];
