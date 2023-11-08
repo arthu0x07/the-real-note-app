f3ac54ff08d52bd602b4dfe896b0cbf8753e8b10
Arthur Silva <arthur.santos.off@gmail.com>
2023-11-08 20:42:01 -0300

    chore: fix jest compatibility with typescript

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,26 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/.jest/setup-tests.js"],
   moduleNameMapper: {
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/.jest/mocks/fileMock.ts',
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/.jest/mocks/fileMock.ts",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "@components/(.*)": "<rootDir>/src/components/$1",
+    "@pages/(.*)": "<rootDir>/src/pages/$1",
+    "@assets/(.*)": "<rootDir>/src/assets/$1",
+    "@mocks/(.*)": "<rootDir>/src/mocks/$1",
+    "@styles/(.*)": "<rootDir>/src/styles/$1",
+    "@hooks/(.*)": "<rootDir>/src/hooks/$1",
+    "@theme/(.*)": "<rootDir>/src/theme/$1",
+    "@types/(.*)": "<rootDir>/src/types/$1",
+    "types/(.*)": "<rootDir>/src/types/$1",
+    "@context/(.*)": "<rootDir>/src/context/$1",
+    "@utils/(.*)": "<rootDir>/src/utils/$1",
   },
-}
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.tsx$": "ts-jest",
+  },
+};
