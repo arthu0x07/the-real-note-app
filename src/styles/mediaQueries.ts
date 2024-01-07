2030d86a1241df7169bd59a7d4540b2bd93ba590
Arthur Silva <arthur.santos.off@gmail.com>
2024-01-07 01:43:34 -0300

    chore: create a file to manager mediaQueries

---
+++ src/styles/mediaQueries.ts
@@ -0,0 +1,13 @@
+export const breakpoints = {
+  small: "576px",
+  medium: "768px",
+  large: "992px",
+  xlarge: "1200px",
+};
+
+export const mediaQueries = {
+  small: `(max-width: ${breakpoints.small})`,
+  medium: `(max-width: ${breakpoints.medium})`,
+  large: `(max-width: ${breakpoints.large})`,
+  xlarge: `(max-width: ${breakpoints.xlarge})`,
+};
